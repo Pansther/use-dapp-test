761aef3768223f8c374a6a8e134ac9707701fc5c
Werawit <werawit_boat@windowslive.com>
2021-10-13 17:37:50 +0700

    add contract functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEtherBalance, useEthers, useContractFunction } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+import { utils, Contract } from "ethers";
 
-function App() {
+export function App() {
+  const abi = [
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "num", type: "uint256" }],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+  const contractInterface = new utils.Interface(abi);
+  const contract = new Contract(
+    "0x364a93FA436C7d8a967B151cb76f8Be2b2D4f6f5",
+    contractInterface
+  );
+
+  const { activateBrowserWallet, deactivate, account } = useEthers();
+  const userBalance = useEtherBalance(account);
+  const { send: retrieve } = useContractFunction(contract, "retrieve");
+  const { send: store } = useContractFunction(contract, "store");
+
+  async function sendRetrieve() {
+    const res = await retrieve();
+    console.log("res", res);
+  }
+
+  function sendStore() {
+    store(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!account && (
+        <button onClick={() => activateBrowserWallet()}> Connect </button>
+      )}
+      {account && (
+        <>
+          <button onClick={deactivate}> Disconnect </button>
+          <p>Account: {account}</p>
+          {userBalance && <p>Balance: {formatEther(userBalance)} BNB</p>}
+          <button onClick={sendRetrieve}>Retrieve</button>
+          <button onClick={sendStore}>Store</button>
+        </>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChainId, DAppProvider } from "@usedapp/core";
+
+const config = {
+  readOnlyChainId: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]:
+      "https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934",
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
